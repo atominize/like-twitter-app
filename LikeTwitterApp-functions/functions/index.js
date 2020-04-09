6e7264b827cf5e0cc90ceb914548667f33c7fb1f
atominize <atominize@gmail.com>
2020-04-09 18:23:33 +0000

    Fixed bug, credentials not loaded

--- LikeTwitterApp-functions/functions/index.js
+++ LikeTwitterApp-functions/functions/index.js
@@ -1,13 +1,17 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const serviceAccount = require('../key/serviceAccountKey.json');
 
-admin.initializeApp();
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://liketwitterapp.firebaseio.com"
+  });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    response.send(functions.config().firebase);
 });
 
 exports.getScreams = functions.https.onRequest((req, res) => {
