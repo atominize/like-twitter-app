31fd3ac8bdd412918ca8a79060fea5305c1b8ab6
atominize <atominize@gmail.com>
2020-04-12 17:44:36 +0000

    rearrange some codes

---
+++ LikeTwitterApp-functions/functions/utils/admin.js
@@ -0,0 +1,12 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = require('../../key/serviceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://liketwitterapp.firebaseio.com"
+  });
+
+const db = admin.firestore();
+
+module.exports = { admin, db}
