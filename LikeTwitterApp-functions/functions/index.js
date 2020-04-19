0465fb68fb9cd9418d277fc7d24617b5034872a3
atominize <atominize@gmail.com>
2020-04-19 10:57:07 +0000

    finished with like and unlike scream

--- LikeTwitterApp-functions/functions/index.js
+++ LikeTwitterApp-functions/functions/index.js
@@ -4,8 +4,17 @@ const app = require('express')();
 
 const FbAuth = require('./utils/fbAuth');
 
-const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams');
-const { signup, login, addUserDetails, getAuthUserDetails, uploadImage } = require('./handlers/users');
+const { getAllScreams,
+        postOneScream,
+        getScream, 
+        commentOnScream,
+        likeScream,
+        unlikeScream } = require('./handlers/screams');
+const { signup,
+        login, 
+        addUserDetails, 
+        getAuthUserDetails, 
+        uploadImage } = require('./handlers/users');
 
 
 // scream routes
@@ -13,6 +22,9 @@ app.get('/screams', getAllScreams);
 app.post('/scream', FbAuth, postOneScream);
 app.get('/scream/:screamId', getScream);
 app.post('/scream/:screamId/comment', FbAuth, commentOnScream);
+app.get('/scream/:screamId/like', FbAuth, likeScream);
+app.get('/scream/:screamId/unlike', FbAuth, unlikeScream);
+
 
 //Signup route
 app.post('/signup', signup);
