4a0fb05db5bcc4dfc75a70969e1c069aeb9b1444
atominize <atominize@gmail.com>
2020-04-20 15:11:59 +0000

    finished with notifications, not tested

--- LikeTwitterApp-functions/functions/index.js
+++ LikeTwitterApp-functions/functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions');
 
+const { db } = require('./utils/admin');
+
 const app = require('express')();
 
 const FbAuth = require('./utils/fbAuth');
@@ -9,7 +11,8 @@ const { getAllScreams,
         getScream, 
         commentOnScream,
         likeScream,
-        unlikeScream } = require('./handlers/screams');
+        unlikeScream,
+        deleteScream } = require('./handlers/screams');
 const { signup,
         login, 
         addUserDetails, 
@@ -24,6 +27,7 @@ app.get('/scream/:screamId', getScream);
 app.post('/scream/:screamId/comment', FbAuth, commentOnScream);
 app.get('/scream/:screamId/like', FbAuth, likeScream);
 app.get('/scream/:screamId/unlike', FbAuth, unlikeScream);
+app.delete('/scream/:screamId', FbAuth, deleteScream);
 
 
 //Signup route
@@ -40,3 +44,63 @@ app.get('/user', FbAuth, getAuthUserDetails)
 app.post('/user/image', FbAuth, uploadImage)
 
 exports.api = functions.https.onRequest(app);
+
+exports.createNotificationOnLike = functions.firestore.document('likes/{id}')
+    .onCreate((snapshot) => {
+        db.doc(`/screams/${snapshot.data().screamId}`).get()
+            .then(doc => {
+                if(doc.exists) {
+                    return db.doc(`/notification/${snapshot.id}`).set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: 'like',
+                        read: false,
+                        screamId: doc.id
+                    });
+                }
+            })
+            .then(() => {
+                return;
+            })
+            .then(err => {
+                console.error(err);
+                return;
+            });
+    });
+
+exports.deleteNotificationOnUnlike = functions.firestore.document('likes/{id}')
+    .onDelete((snapshot) => {
+        db.doc(`/notification/${snapshot.id}`).delete()
+            .then(() => {
+                return;
+            })
+            .then(err => {
+                console.error(err);
+                return;
+            });
+    });
+
+exports.createNotificationOnComment = functions.firestore.document('comments/{id}')
+    .onCreate((snapshot) => {
+        db.doc(`/screams/${snapshot.data().screamId}`).get()
+            .then(doc => {
+                if(doc.exists) {
+                    return db.doc(`/notification/${snapshot.id}`).set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: 'comment',    
+                        read: false,
+                        screamId: doc.id
+                    });
+                }
+            })
+            .then(() => {
+                return;
+            })
+            .then(err => {
+                console.error(err);
+                return;
+            });
+    });
